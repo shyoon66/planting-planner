d4783c855beb03992522e80c1a48e17f4078fc33
harry.yoon <shyoon0606@gmail.com>
2020-07-22 23:52:25 +0900

    설정 기능 추가
    1. 오픈소스 라이선스 작업중

--- app/src/main/java/com/yoonbae/planting/planner/OpenSourceLicenseActivity.java
+++ app/src/main/java/com/yoonbae/planting/planner/OpenSourceLicenseActivity.java
@@ -2,12 +2,16 @@ package com.yoonbae.planting.planner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class OpenSourceLicenseActivity extends AppCompatActivity {
 
@@ -18,10 +22,14 @@ public class OpenSourceLicenseActivity extends AppCompatActivity {
         initToolBar();
         Intent intent = getIntent();
         String openSourceName = intent.getStringExtra("openSourceName");
-        initActionBar(openSourceName);
-        String license = intent.getStringExtra("license");
+        initActionBar();
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText(openSourceName);
+        String license = Optional.ofNullable(intent.getStringExtra("license")).orElse("");
+        license = license.replaceAll("\n", Objects.requireNonNull(System.getProperty("line.separator")));
         TextView licenseTextView = findViewById(R.id.license);
         licenseTextView.setText(license);
+        licenseTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
@@ -41,7 +49,7 @@ public class OpenSourceLicenseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    private void initActionBar(String openSourceName) {
+    private void initActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar == null) {
             return;
@@ -50,6 +58,5 @@ public class OpenSourceLicenseActivity extends AppCompatActivity {
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle(openSourceName);
     }
 }
