472d5e856cb42c9aba8d2c5fd88075a1b203b57b
harry.yoon <shyoon0606@gmail.com>
2020-08-01 21:27:56 +0900

    설정 기능 추가
    1. 푸시 알람, 진동, 소리 설정 기능 추가

--- app/src/main/java/com/yoonbae/planting/planner/SettingsActivity.java
+++ app/src/main/java/com/yoonbae/planting/planner/SettingsActivity.java
@@ -1,13 +1,9 @@
 package com.yoonbae.planting.planner;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -24,7 +20,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment(this))
+                .replace(R.id.settings, new SettingsFragment())
                 .commit();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -34,11 +30,6 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
-        private Activity activity;
-
-        public SettingsFragment(Activity activity) {
-            this.activity = activity;
-        }
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -49,29 +40,6 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             }));
-
-            Optional<Preference> optVibration = Optional.ofNullable(findPreference("vibration"));
-            optVibration.ifPresent(vibration -> vibration.setOnPreferenceChangeListener((preference, newValue) -> {
-                AudioManager audioManager = (AudioManager) activity.getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-                if ((boolean) newValue) {
-                    audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-                } else {
-                    audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                }
-                return true;
-            }));
-        }
-
-        private void showSettingsDialog(Activity activity) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            builder.setTitle("권한 필요");
-            builder.setMessage("이 기능을 사용하려면 이 앱에 권한이 필요합니다. 앱 설정에서 권한을 설정할 수 있습니다.");
-            builder.setPositiveButton("GOTO SETTINGS", (dialog, which) -> {
-                Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
-                startActivity(intent);
-            });
-            builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-            builder.show();
         }
     }
 
--- app/src/main/java/com/yoonbae/planting/planner/alarm/AlarmReceiver.java
+++ app/src/main/java/com/yoonbae/planting/planner/alarm/AlarmReceiver.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.core.app.NotificationCompat;
@@ -30,8 +31,9 @@ public class AlarmReceiver extends BroadcastReceiver {
             return;
         }
 
+        boolean isVibration = sharedPreferences.getBoolean("vibration", true);
+        NotificationChannel notificationChannel = getNotificationChannel(isVibration);
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationChannel notificationChannel = getNotificationChannel();
         notificationManager.createNotificationChannel(notificationChannel);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
         Bundle bundle = intent.getExtras();
@@ -43,14 +45,15 @@ public class AlarmReceiver extends BroadcastReceiver {
             .setContentText(name + " 물주기 알람입니다.")
             .setDefaults(Notification.DEFAULT_ALL)
             .setAutoCancel(true)
-            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
             .setSmallIcon(android.R.drawable.btn_star)
             .setContentIntent(pendingIntent);
 
+        boolean isSound = sharedPreferences.getBoolean("sound", true);
+        builder.setSound(getSound(isSound));
         notificationManager.notify(alarmId, builder.build());
     }
 
-    private NotificationChannel getNotificationChannel() {
+    private NotificationChannel getNotificationChannel(boolean isVibration) {
         NotificationChannel notificationChannel =
             new NotificationChannel(NOTIFICATION_CHANNEL_WATER_ALARM_ID, NOTIFICATION_CHANNEL_WATER_ALARM_NAME,
                                     NotificationManager.IMPORTANCE_DEFAULT);
@@ -58,10 +61,23 @@ public class AlarmReceiver extends BroadcastReceiver {
         notificationChannel.setDescription("water Alarm");
         notificationChannel.enableLights(true);
         notificationChannel.setLightColor(Color.GREEN);
-        notificationChannel.enableVibration(true);
-        notificationChannel.setVibrationPattern(new long[] {100, 200, 100, 200});
+
+        if (isVibration) {
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[] {100, 200, 100, 200});
+        } else {
+            notificationChannel.enableVibration(false);
+            notificationChannel.setVibrationPattern(null);
+        }
         notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
         return notificationChannel;
+    }
+
+    private Uri getSound(boolean isSound) {
+        if (isSound) {
+            return RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        return null;
     }
 
     private int getAlarmId(Bundle bundle) {
